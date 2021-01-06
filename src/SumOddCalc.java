41f6a5c857b972de997cbbb084a275ba53f611d1
xtreme6007 <prestonnichols96@gmail.com>
2021-01-06 11:57:35 -0600

    finished sumODd method

--- src/SumOddCalc.java
+++ src/SumOddCalc.java
@@ -1,22 +1,32 @@
 public class SumOddCalc {
-    public static boolean isOdd(int number){
-        if (number < 0){
-            return false;
-        } else if((number % 2 != 0)){
-            return true;
-        }
-        return false;
 
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        } else if ((number % 2 != 0)) {
+            return true;
+        } else {
+            return false;
+        }
 
 
     }
 
-    public static int sumOdd(int start, int end){
+    public static int sumOdd(int start, int end) {
         int sum = 0;
-        for(i = start; i < end; i++){
+        if (end < start || start <= 0 || end <= 0) {
+            sum = -1;
+        } else {
+            for (int i = start; i <= end; i++) {
+                if (isOdd(i)) {
+                    sum += i;
 
+                }
+
+            }
         }
-
+        System.out.println(sum);
+        return sum;
     }
 
 }
