dc2eb3ba5b964b592f51837b0e190d54fc48d2fc
xtreme6007 <prestonnichols96@gmail.com>
2021-01-06 11:59:20 -0600

    added in more examples

--- src/main.java
+++ src/main.java
@@ -1,6 +1,11 @@
 public class main {
     public static void main(String[] args){
-        SumOddCalc.sumOdd(1, 11);
+        SumOddCalc.sumOdd(1, 100);
+        SumOddCalc.sumOdd(-1, 100);
+        SumOddCalc.sumOdd(100, 100);
+        SumOddCalc.sumOdd(13, 13);
+        SumOddCalc.sumOdd(100, -100);
+        SumOddCalc.sumOdd(100, 1000);
 
     }
 }
