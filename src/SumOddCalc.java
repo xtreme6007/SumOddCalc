f13c88a8fd8d859a86a36c060f35c893b3687973
xtreme6007 <prestonnichols96@gmail.com>
2021-01-06 11:37:55 -0600

    added isOdd method to test if number is odd or not

--- src/SumOddCalc.java
+++ src/SumOddCalc.java
@@ -1,1 +1,16 @@
+public class SumOddCalc {
+    public static boolean isOdd(int number){
+        if (number < 0){
+            return false;
+        } else if((number % 2 != 0)){
+            return true;
+        }
+        return false;
 
+
+
+    }
+
+
+
+}
